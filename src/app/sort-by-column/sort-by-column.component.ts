779483f29645e3e222464560c6790512ec4589b3
prithvikreddy <vuppulaprithvireddy@gmail.com>
2017-09-28 15:54:30 -0400

    created sorting method and sort data with table head property

--- src/app/sort-by-column/sort-by-column.component.ts
+++ src/app/sort-by-column/sort-by-column.component.ts
@@ -8,14 +8,25 @@ import { DataService } from '../services/data.service';
 })
 export class SortByColumnComponent implements OnInit {
     dataArray: Array<any>;
-
-
+    sort: string;
   constructor(private dataService: DataService) { }
-
+  //generating table data using dataService
   ngOnInit() {
     this.dataService.getJsonData().subscribe(data => {
       this.dataArray = data;
     })
   }
+  //sorting method to arrange displayed data in assending/descending order onClick event occurs in table head
+  sorting(sortelement: any) {
+        if (this.sort == "sortedBy" + sortelement) {//rev sort
+            this.dataArray.reverse();
+            this.sort = "vertsortedBy" + sortelement;
+            
+        }
+        else {//normal sort
+            this.dataArray.sort(function (a, b) { return (a[sortelement] > b[sortelement]) ? 1 : ((b[sortelement] > a[sortelement]) ? -1 : 0); });
+            this.sort = "sortedBy" + sortelement;
+        }
+    }
 
 }
